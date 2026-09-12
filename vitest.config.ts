b4d37e90319e4bbdf5aebf01f6cee09d4039c553
Matt Drake <113171251+mattcdrake@users.noreply.github.com>
2026-09-12 15:00:24 -0700

    test: silence console output for passing tests (#390)

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import { WxtVitest } from 'wxt/testing/vitest-plugin';
 
 export default defineConfig({
   test: {
+    silent: 'passed-only',
     mockReset: true,
     restoreMocks: true,
     setupFiles: './test/setup.ts',
